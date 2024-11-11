d274cb95bb3302069d4a5d771f54ab1f876dc940
hazhirmohammadi <hazhir6690@gmail.com>
2024-11-11 17:20:29 +0330

    store

--- frontend/components/crad-line/file-and-directors/Directory.tsx
+++ frontend/components/crad-line/file-and-directors/Directory.tsx
@@ -4,21 +4,21 @@ import { GoFileDirectoryFill } from "react-icons/go";
 
 const Directory = () => {
   const items = [
-    { id: 1, name: "Laptop", category: "Electronics", price: 999.99 },
-    { id: 2, name: "Coffee Maker", category: "Home Appliances", price: 49.99 },
-    { id: 3, name: "Desk Chair", category: "Furniture", price: 150.0 },
-    { id: 4, name: "Smartphone", category: "Electronics", price: 799.99 },
-    { id: 5, name: "Headphones", category: "Accessories", price: 199.99 },
-    { id: 11, name: "Laptop", category: "Electronics", price: 999.99 },
-    { id: 22, name: "Coffee Maker", category: "Home Appliances", price: 49.99 },
-    { id: 33, name: "Desk Chair Desk Chair", category: "Furniture", price: 150.0 },
-    { id: 44, name: "Smartphone", category: "Electronics", price: 799.99 },
-    { id: 55, name: "Headphones", category: "Accessories", price: 199.99 },
-    { id: 16, name: "Laptop", category: "Electronics", price: 999.99 },
-    { id: 222, name: "Coffee Maker", category: "Home Appliances", price: 49.99 },
-    { id: 333, name: "Desk Chair", category: "Furniture", price: 150.0 },
-    { id: 4444, name: "Smartphone", category: "Electronics", price: 799.99 },
-    { id: 5555, name: "Headphones", category: "Accessories", price: 199.99 },
+    { id: 1, name: "Laptop", category: "Electronics", price: 999.99 ,path:"Laptop"},
+    { id: 2, name: "Coffee Maker", category: "Home Appliances", price: 49.99 ,path:"Coffee"},
+    { id: 3, name: "Desk Chair", category: "Furniture", price: 150.0 ,path:"Smartphone"},
+    { id: 4, name: "Smartphone", category: "Electronics", price: 799.99 ,path:"Headphones"},
+    { id: 5, name: "Headphones", category: "Accessories", price: 199.99 ,path:"Laptop"},
+    { id: 11, name: "Laptop", category: "Electronics", price: 999.99 ,path:""},
+    { id: 22, name: "Coffee Maker", category: "Home Appliances", price: 49.99 ,path:""},
+    { id: 33, name: "Desk Chair Desk Chair", category: "Furniture", price: 150.0 ,path:""},
+    { id: 44, name: "Smartphone", category: "Electronics", price: 799.99 ,path:""},
+    { id: 55, name: "Headphones", category: "Accessories", price: 199.99 ,path:""},
+    { id: 16, name: "Laptop", category: "Electronics", price: 999.99 ,path:""},
+    { id: 222, name: "Coffee Maker", category: "Home Appliances", price: 49.99 ,path:""},
+    { id: 333, name: "Desk Chair", category: "Furniture", price: 150.0 ,path:""},
+    { id: 4444, name: "Smartphone", category: "Electronics", price: 799.99 ,path:""},
+    { id: 5555, name: "Headphones", category: "Accessories", price: 199.99 ,path:""},
   ];
   return (
     <div className="flex flex-col overflow-y-auto  max-h-[500px]">
